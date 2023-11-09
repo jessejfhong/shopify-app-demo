f6dd0e4ee663d0daa84a6b72bfecb786ad432b77
jesse <121727576+jessejfhong@users.noreply.github.com>
2023-11-09 12:34:42 +0800

    upgrade app bridge

--- webapp/src/bootstrap.js
+++ webapp/src/bootstrap.js
@@ -1,19 +1,10 @@
 import './main.css';
-import createApp from '@shopify/app-bridge';
-import { getSessionToken } from "@shopify/app-bridge/utilities";
 import { Elm } from './Main.elm';
-
-// const appBridge = createApp({
-//     apiKey: '', // looper app
-//     host: new URLSearchParams(location.search).get('host'),
-//     forceRedirect: true
-// });
 
 const app = Elm.Main.init({
     flags: window.location.origin
 });
 
 // app.ports.requestSessionToken.subscribe(async () => {
-//     const sessionToken = await getSessionToken(appBridge);
-//     app.ports.sessionToken.send(sessionToken);
+//     app.ports.sessionToken.send(await shopify.idToken());
 // });
